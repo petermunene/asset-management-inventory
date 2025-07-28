e6f7728bde4a0bd68c14260a62a0a82e171b5b09
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-07-28 14:04:12 +0300

    adds login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EmployeeDashboard from './components/EmployeeDashboard';
 import HomePage from './components/HomePage'  ;
+import Login from './components/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/*" element={<EmployeeDashboard />} />
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/*" element={<Login  />} />
+        <Route path="/homepage" element={<Login />} />
       </Routes>
     </Router>
 );
